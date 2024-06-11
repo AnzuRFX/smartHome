07833b2ba405edcde502ea2a4781a9a7ec6a1f95
Talziar <71977032+Talziar@users.noreply.github.com>
2024-06-11 10:53:45 +0300

    Added enhanced light and thermo sensor system

--- smart_home.ino
+++ smart_home.ino
@@ -2,15 +2,17 @@
 #include <math.h>
 #define PHOTO_PIN A0
 #define THERMO_PIN A1
+#define THERMO_LED_PIN 2
 
 
 
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-float brightness, thermoPinValue, temperature;
+float brightness = 0.0, thermoPinValue, temperature, lastBrightValue;
+bool thermoOn = false;
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.print("   SMART HOME   ");
+  lcd.print("  GOOD DAY      ");
 }
 
 void setLcdInfo(String parameter, float value, String unit){
@@ -27,7 +29,25 @@ void setLcdInfo(String parameter, float value, String unit){
 void loop() {
   thermoPinValue = analogRead(THERMO_PIN) * 5.0 / 1024.0;
   temperature = -14.46 * log((10000.0 * thermoPinValue) / (5.0 - thermoPinValue) / 27074.0);
+  lastBrightValue = brightness;
   brightness = (1024.0 - analogRead(PHOTO_PIN)) / 1024.0 * 100;
+
+  if(lastBrightValue > 25.0 && brightness > 25.0){
+    lcd.setCursor(0, 0);
+    lcd.print("  GOOD DAY      ");
+  }else if (lastBrightValue <= 25.0 && brightness <= 25.0){
+    lcd.setCursor(0, 0);
+    lcd.print("  GOOD EVENING  ");
+  }
+
+  if(thermoOn && temperature <= 15.0){
+    digitalWrite(THERMO_LED_PIN, LOW);
+    thermoOn = !thermoOn;
+  } 
+  if(!thermoOn && temperature >= 17.0){
+    digitalWrite(THERMO_LED_PIN, HIGH);
+    thermoOn = !thermoOn;
+  } 
 
   setLcdInfo("TEMP:  ", temperature, "C");
   setLcdInfo("LIGHT: ", brightness, "%");
